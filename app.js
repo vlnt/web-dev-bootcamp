e503647fdeb257e0cb2d22a3e19d6c9c24484f2e
Valentyn Mirosh <vlnt@ua.fm>
2022-05-23 23:46:28 +0300

    error pages

--- app.js
+++ app.js
@@ -59,7 +59,24 @@ app.post('/recommend', function(req, res){
 
   app.get('/restaurants/:id', function(req, res){
       const restaurantId = req.params.id
-      res.render('restaurant-detail', {rid: restaurantId})
+      const filePath = path.join(__dirname, 'data', 'restaurants.json')
+      const fileData = fs.readFileSync(filePath)
+      const storedRestaurants = JSON.parse(fileData)
+      for(const restaurant of storedRestaurants){
+          if(restaurant.id === restaurantId){
+            return res.render('restaurant-detail', {restaurant: restaurant})
+          }
+      }
+      res.status(404).render('404')
+  })
+
+  app.use(function(req, res){
+      res.status(404).render('404')
+  })
+
+  app.use(function(error, req, res, next){
+      res.status(500).render('500')
+      console.log(error)
   })
   
 
