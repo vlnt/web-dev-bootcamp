230c33d32d4f4a1f326c040455965d404ea54f88
Valentyn Mirosh <vlnt@ua.fm>
2022-11-19 21:27:41 +0200

    working caching photos

--- redis_ex/server.js
+++ redis_ex/server.js
@@ -3,9 +3,7 @@ const axios = require('axios')
 const cors = require('cors')
 const Redis = require('redis')
 
-const redisClient = Redis.createClient({
-    url: 'redis://localhost:6379'
-})
+const redisClient = Redis.createClient()
 redisClient.connect()
 
 const DEFAULT_EXPIRATION = 3600
@@ -14,15 +12,36 @@ const app = express()
 app.use(cors())
 
 app.get("/photos", async (req, res) => {
-    const albumId = req.query.albumId
-    const {data} = await axios.get(
-        "https://jsonplaceholder.typicode.com/photos",
-        {params: {albumId}}
-    )
-    //redisClient.connect()
-    redisClient.setEx('photos', DEFAULT_EXPIRATION, JSON.stringify(data) )
-    res.json(data)
-})
+   // try{
+        console.log('getting photos')
+        const photos = await redisClient.get("photos")
+        if(photos != null){
+            res.json(photos)
+        } else {
+            const {data} = await axios.get("https://jsonplaceholder.typicode.com/photos")
+                           redisClient.setEx('photos', DEFAULT_EXPIRATION, JSON.stringify(data) )
+                           res.json(data)
+                        }
+        }
+)      
+        
+    //     redisClient.get("photos", async (error, photos) => {
+    //         if(error) console.log(error)
+    //         if (photos != null){
+    //              res.json(JSON.parse(photos))
+    //         } else {
+    //             const {data} = await axios.get(
+    //                 "https://jsonplaceholder.typicode.com/photos"
+    //             )
+    //             redisClient.setEx('photos', DEFAULT_EXPIRATION, JSON.stringify(data) )
+    //         }
+    //         res.json(data)
+    //     })
+    // }catch(error){
+    //     res.status(500).send({error:error.message})
+    // }
+    
+//})
 
 app.get("/photos/:id", async (req, res) => {
     const {data} = await axios.get(
